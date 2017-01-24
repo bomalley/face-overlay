0f3c14926a9bb2a62719724d3cb4e4110048772a
Bryan O'Malley <bomalley@fingerpaintmarketing.com>
2017-01-24 10:24:13 -0500

    Working face detection

--- faceoverlay/ViewController.m
+++ faceoverlay/ViewController.m
@@ -10,8 +10,13 @@
 @import CoreImage;
 @import ImageIO;
 
+
+
+
 @interface ViewController () {
 	CIDetector *faceDetector;
+
+	NSURL *photoURL;
 }
 
 @property (weak, nonatomic) IBOutlet UIImageView *photo;
@@ -27,28 +32,27 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 
+	photoURL = [[NSBundle mainBundle] URLForResource: @"michael" withExtension: @"jpg"];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
 	[super viewWillAppear: animated];
 
-//	[self setupFaceDetector];
+	[photo setImage: [UIImage imageWithData: [NSData dataWithContentsOfURL: photoURL]]];
+	[self setupFaceDetector];
 }
 
 - (void)setupFaceDetector {
-//	CIContext *context = [CIContext context];
 	NSDictionary *opts = @{ CIDetectorAccuracy : CIDetectorAccuracyHigh };
 
 	faceDetector = [CIDetector detectorOfType: CIDetectorTypeFace
 									  context: nil
 									  options: opts];
 
-	NSURL *photoURL = [[NSBundle mainBundle] URLForResource: @"bryan" withExtension: @"jpg"];
 	CIImage *ciPhoto = [CIImage imageWithContentsOfURL: photoURL];
 
 	NSLog(@"%@", [ciPhoto properties]);
-	opts = @{ CIDetectorImageOrientation :
-				  [[ciPhoto properties] valueForKey: kCGImagePropertyOrientation] };
+	opts = @{ CIDetectorImageOrientation : [[ciPhoto properties] valueForKey: kCGImagePropertyOrientation] };
 
 
 	// Detect the faces
@@ -66,66 +70,42 @@
 
 	CGFloat ciPhotoWidth = [[[ciPhoto properties] objectForKey: @"PixelWidth"] floatValue];
 	CGFloat ciPhotoHeight = [[[ciPhoto properties] objectForKey: @"PixelHeight"] floatValue];
-	CGFloat widthRatio = 1024/ciPhotoWidth;
-	CGFloat heightRatio = 768/ciPhotoHeight;
+	CGFloat uiPhotoWidth = CGRectGetWidth([photo bounds]);
+	CGFloat uiPhotoHeight = CGRectGetHeight([photo bounds]);
+	CGFloat widthRatio = uiPhotoWidth/ciPhotoWidth;
+	CGFloat heightRatio = uiPhotoHeight/ciPhotoHeight;
 
 
-	fb.origin.x = (fb.origin.x) * widthRatio;
-	fb.origin.y = fb.origin.x * widthRatio;
+	UIView *leftEyeAlt = [[UIView alloc] initWithFrame: CGRectMake(le.x * widthRatio, (ciPhotoHeight - le.y) * heightRatio, 5, 5)];
+	[leftEyeAlt setBackgroundColor: [UIColor whiteColor]];
+	[[self view] addSubview: leftEyeAlt];
 
-	UIView *face = [[UIView alloc] initWithFrame: fb];
-	[face setBackgroundColor: [UIColor redColor]];
-	[[self view] addSubview: face];
-	
+	CGPoint faceCenter;
 
-	CGPoint leftEyePoint;
+	faceCenter.x = ((le.x + re.x + mo.x) / 3.0) * widthRatio;
+	faceCenter.y = (([self fixY: le.y forImageHeight: ciPhotoHeight] + [self fixY: re.y forImageHeight: ciPhotoHeight] + [self fixY: mo.y forImageHeight: ciPhotoHeight]) / 3.0) * heightRatio;
 
-	leftEyePoint.x = ciPhotoWidth - le.x;
-	leftEyePoint.y = ciPhotoHeight - le.y;
-
-	UIView *leftEye = [[UIView alloc] initWithFrame: CGRectMake(leftEyePoint.x * widthRatio, le.y * heightRatio, 5, 5)];
-	[leftEye setBackgroundColor: [UIColor redColor]];
-	[[self view] addSubview: leftEye];
-
-	CGPoint rightEyePoint;
-
-	rightEyePoint.x = ciPhotoWidth - re.x;
-	rightEyePoint.y = ciPhotoHeight - re.y;
-
-	UIView *rightEye = [[UIView alloc] initWithFrame: CGRectMake(rightEyePoint.x * widthRatio, re.y * heightRatio, 5, 5)];
-	[rightEye setBackgroundColor: [UIColor orangeColor]];
-	[[self view] addSubview: rightEye];
-
-	CGPoint mouthPoint;
-
-	mouthPoint.x = ciPhotoWidth - mo.x;
-	mouthPoint.y = ciPhotoHeight - mo.y;
-
-	UIView *mouth = [[UIView alloc] initWithFrame: CGRectMake(mouthPoint.x * widthRatio, mo.y * heightRatio, 5, 5)];
-	[mouth setBackgroundColor: [UIColor blueColor]];
-	[[self view] addSubview: mouth];
-
-	CGRect overlayFrame = [overlay frame];
-
-	CGFloat overlayRatio = CGRectGetHeight(overlayFrame) / CGRectGetWidth(overlayFrame);
-
-	overlayFrame.origin.x = fb.origin.x * widthRatio;
-	overlayFrame.origin.y = fb.origin.y * heightRatio;
-	overlayFrame.size.width = fb.size.width * widthRatio;
-	overlayFrame.size.height = overlayFrame.size.width * overlayRatio;
+	[overlay setCenter: faceCenter];
+//	CGRect overlayFrame = [overlay frame];
 //
-//	overlayFrame.origin.x = 0;
-//	overlayFrame.origin.y = 0;
-//	overlayFrame.size.width = 100;
-//	overlayFrame.size.height = 100;
+//	CGFloat overlayRatio = CGRectGetHeight(overlayFrame) / CGRectGetWidth(overlayFrame);
 //
-	[overlay setFrame: overlayFrame];
+//	overlayFrame.origin.x = fb.origin.x * widthRatio;
+//	overlayFrame.origin.y = fb.origin.y * heightRatio;
+//	overlayFrame.size.width = fb.size.width * widthRatio;
+//	overlayFrame.size.height = overlayFrame.size.width * overlayRatio;
+//
+//	[overlay setFrame: overlayFrame];
 //	[overlay setCenter: CGPointMake(50, 50)];
 //	[overlay setBackgroundColor: [UIColor greenColor]];
+//
+//
+//	NSLog(@"%@", faces);
+//	NSLog(@"%@", ff);
+}
 
-
-	NSLog(@"%@", faces);
-	NSLog(@"%@", ff);
+- (CGFloat)fixY:(CGFloat)y forImageHeight:(CGFloat)height {
+	return height - y;
 }
 
 @end
